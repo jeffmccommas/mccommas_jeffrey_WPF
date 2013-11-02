505385e9a8e978c2b5142341c472251a4c6edae0
Jeff McCommas <jeffmccommas@gmail.com>
2013-11-02 11:47:39 -0400

    New JS File

--- Expressions_worksheet/js/scripts.js
+++ Expressions_worksheet/js/scripts.js
@@ -1,4 +1,9 @@
-/* Jeffrey McCommas
-Nov. 2nd 2013
-Activity: Expression Worksheet
+/**
+ * Created by sales on 11/2/13.
  */
+/* Jeffrey McCommas
+ Nov. 2nd 2013
+ Activity: Expression Worksheet
+ */
+
+$ ()
