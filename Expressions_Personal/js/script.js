4107f6a1be258aa864eff5f35c85e0f96101306b
Jeffrey McCommas <jeffmccommas@gmail.com>
2013-11-03 18:17:41 -0500

    Just about ready

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -18,8 +18,10 @@ if (!isNaN(startOdometer)) { // Check that the user has entered a valid number, 
 // Record the amount of gas it took to refill the tank.
 var endingOdometer = prompt("Drive your car as you normally would, and let your gas tank deplete to at least a half of a tank of gas.\n\nWhen you are ready enter the new odometer reading.");
 
+
+// Check that the user has entered a valid number, if is not a number, alert user
 if (!isNaN(endingOdometer)) {
-    alert("This is  a valid number. You can proceed to next step!! "); // Check that the user has entered a valid number, if is not a number, alert user
+    alert("This is  a valid number. You can proceed to next step!! ");
 } else {
     alert("This is not a valid number. Please enter a valid number ");
 }
@@ -27,8 +29,10 @@ if (!isNaN(endingOdometer)) {
 // Drive your car as you normally would, and let your gas tank deplete to at least a half of a tank of gas.
 var gasFillUp = prompt("Record the amount of gas it took to refill the tank and enter the amount");
 
+
+// Check that the user has entered a valid number, if is not a number, alert user
 if (!isNaN(gasFillUp)) {
-    alert("This is a valid number. You can proceed to next step!! "); // Check that the user has entered a valid number, if is not a number, alert user
+    alert("This is a valid number. You can proceed to next step!! ");
 } else {
     alert("This is not a valid number. Please enter a valid number ");
 }
@@ -40,8 +44,6 @@ Divide the miles traveled by the amount of gallons it took to refill the tank. T
 
 var milesDriven = endingOdometer - startOdometer;
 var gasMileage =  milesDriven / gasFillUp;
-
-var results = gasMileage;
 
 gasMileage = Math.round(gasMileage); // Rounds to the nearest whole number
 
