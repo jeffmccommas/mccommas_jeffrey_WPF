00c1268e8d853f9e365013f4a2927ce68a4044f7
Jeffrey McCommas <jeffmccommas@gmail.com>
2013-11-13 05:55:32 -0500

    Conditional WS

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -35,57 +35,77 @@
 /******************** Group 2: Multiple Results ********************/
 /******************** Check the Login *****************************/
 
-var newUser = prompt("Enter a UserName for this account. Must be at least 6 characters.", '').toLowerCase();
-var newPassword = prompt("Enter a password for this account . Must be at least 6 characters", '').toLowerCase();
-
-userName = newUser;
-userPass = newPassword;
-
-alert("Congratulations, your account has been created. Click OK to log in to your newly created account");
-
-
-var checkUser = prompt("Enter your username for this account.", '');
-
-if (checkUser === userName) {
-    alert("Welcome, " +userName+ "!");
-    console.log("Welcome, " +userName+ "!");
-
-} else {
-
-    alert("User not found. Try again.");
-    console.log("User not found. Try again.");
-}
-}
-var checkPass = prompt("Enter your password for this account.", '');
-
-if (checkPass === userPass) {
-    alert("Success. You have entered the correct password");
-    console.log("Success. You have entered the correct password");
-} else {
-
-    alert("Password does not match our records.");
-    console.log("Password does not match our records.");
-
-}
-
-
-console.log(userName);
-
-console.log(userPass);
+//var newUser = prompt("Enter a UserName for this account. Must be at least 6 characters.", '').toLowerCase();
+//var newPassword = prompt("Enter a password for this account . Must be at least 6 characters", '').toLowerCase();
+//
+//userName = newUser;
+//userPass = newPassword;
+//
+//alert("Congratulations, your account has been created. Click OK to log in to your newly created account");
+//
+//
+//var checkUser = prompt("Enter your username for this account.", '');
+//
+//if (checkUser === userName) {
+//    alert("Welcome, " +userName+ "!");
+//    console.log("Welcome, " +userName+ "!");
+//
+//} else {
+//
+//    alert("User not found. Try again.");
+//    console.log("User not found. Try again.");
+//
+//}
+//var checkPass = prompt("Enter your password for this account.", '');
+//
+//if (checkPass === userPass) {
+//    alert("Success. You have entered the correct password");
+//    console.log("Success. You have entered the correct password");
+//} else {
+//
+//    alert("Password does not match our records.");
+//    console.log("Password does not match our records.");
+//
+//}
+//
+//
+//console.log(userName);
+//
+//console.log(userPass);
 
 
 
 /******************** Group 3: Multiple Conditions ********************/
+
+/*The local movie theater in town has a ticket price of $12.00 but if you are a senior (55 and older), under 10 you get the discounted price of $7.00. In addition, if a customer is seeing a movie between 3pm and 5pm they can also get the discounted price.
+Determine which of the two prices the customer is eligible for.*/
+
 /******************** Movie Ticket Price *****************************/
 
+var ticketCost = 12;
+var senior = "Senior Citizen";
+var regular = "Regular ticket price";
+var kids = "Child Under the age of 10";
+var specialTime = "Discount ticket during the early bird hours of 3 and 5 PM";
+var movieDiscount = 7;
+
+var age = Number(prompt("Welcome to our movies online purchase program.\nTo begin enter your age and click OK." , ''));
+
+var time = Number(prompt("Enter the time of day tou want to see the movie. Movies tart on the hour between 12 and 10 each day and we hve a discount for children under ten and seniors 55 and over between the hours of 3 and 5", ''));
+
+if (age >= 55 || age <= 10) {
+   var newCost =  ticketCost - movieDiscount;
+    confirm("The ticket price is $" +newCost+ " For one " +senior+ " or " +kids+ "");
+    console.log("The ticket price is $" +newCost+ " For one " +senior+ " or " +kids+ "");
+
+} else if (time >= 3 && time <= 5) {
+    var newDiscount = ticketCost - movieDiscount;
+    confirm("The ticket price is $" +newDiscount+ " For one " +specialTime+ "");
+    console.log("The ticket price is $" +newDiscount+ " For one " +specialTime+ "");
 
 
-/*
- The local movie theater in town has a ticket price of $12.00 but if you are a senior (55 and older), under 10 you get the discounted price of $7.00. In addition, if a customer is seeing a movie between 3pm and 5pm they can also get the discounted price.
- Determine which of the two prices the customer is eligible for.
- Given:
- Time of Movie (Assume whole numbers here)
- Age of the customer
- Result To Print Out:
- “The ticket price is X”
- */
+} else {
+
+    confirm("The ticket price is $" +ticketCost+ " For one " +regular+ "");
+    console.log("The ticket price is $" +ticketCost+ " For one " +regular+ "");
+}
