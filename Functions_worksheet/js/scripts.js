239ef8ab7cc6fcc6094b2b64e6c298f978b96b51
Jeff McCommas <jeffmccommas@gmail.com>
2013-11-16 12:22:00 -0500

    calculate the circumference

--- Functions_worksheet/js/scripts.js
+++ Functions_worksheet/js/scripts.js
@@ -1,3 +1,26 @@
-/**
- * Created by sales on 11/16/13.
+// JavaScript
+/* Jeffrey McCommas
+ Nov. 16th 2013
+ Activity: Functions Worksheet
  */
+
+// Circumference - calculate the circumference of a circle
+
+//store and display the value of PI:
+var pi = Math.PI;
+var calculateCircumference;
+var resultString;
+
+var textRadius = (parseFloat(prompt("Enter length of radius:" , '')));
+
+if (!isNaN(textRadius)) {
+    console.log(textRadius);
+} else {
+    alert("Looks like you didn't enter a number, please try again");
+
+}
+
+
+
+
+// Stung! - Bee sting function
