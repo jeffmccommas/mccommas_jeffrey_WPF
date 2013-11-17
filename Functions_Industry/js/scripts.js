d90be013a2ebcb88ad44da33eb9ce93b33e80d43
Jeffrey McCommas <jeffmccommas@gmail.com>
2013-11-17 13:19:48 -0500

    completed w/o comments

--- Functions_Industry/js/scripts.js
+++ Functions_Industry/js/scripts.js
@@ -6,50 +6,51 @@
 
 
 /////////// Cooking Units Converter ////////////////////
-///////////// U.S. Kitchen Units ////////////
+///////////// U.S. Kitchen Units //////////////////////
 
 /// Givens containing the unit values
+
 var tbsp = .5;
 var cup = 8;
 var pint = 16;
 var quart = 32;
+var final = final;
 
+function converter(){
 
+    if (units === "quart"){
+        final = ounces / quart;
+    } else if (units === "pint"){
+        final = ounces / pint;
+    } else if (units === "cup"){
+        final = ounces /cup;
+    } else if (units === "tbsp"){
+        final = ounces / tbsp;
+
+    }
+
+    return final;
+}
 
 
 var answer = confirm("This is a simple Javascript program that will help you convert U.S. ounces to kitchen units.\n\nExample would be if you need to know how many Tablespoons 32 ounces is for your recipe.\n\nIf you wish to continue click the OK button. If not click cancel!!");
 
 
 if (answer === true) {
-    var ounces = (parseFloat(prompt("Enter the amount of ounces you wish to convert. Example 64", '64')));
-    console.log(ounces);
-    var units = prompt("Enter the unit you want to convert the ounces to.\n\nChoices are quart, pint, cup or tbsp", 'tbsp');
-     console.log(units);
+    var ounces = (parseFloat(prompt("Enter the amount of ounces you wish to convert. Example 64" ,64)));
 
-    var final = units / ounces;
-    function converter(final){
+    var units = prompt("Enter the unit you want to convert the ounces to.\n\nChoices are quart, pint, cup or tbsp", '');
 
-        if (units === quart){
-            final = units / ounces;
-        } else if (units === pint){
-            final = units / ounces;
-        } else if (units === cup){
-            final = units / ounces;
-        } else if (units === tbsp){
-            final = tbsp / ounces;
-            console.log(final)
-        }
 
-        return final;
-    }
+ converter();
 
-var results = (ounces + " Ounce(s) is equivalent to " + final + " " + units + "")
+   var results = (ounces + " Ounce(s) is equivalent to " + final + " " + units + "(s)")
 
 
 } else {
     alert("Thanks for visiting!! Bye!!")
 }
-converter(final);
+
 console.log(results);
 
 
