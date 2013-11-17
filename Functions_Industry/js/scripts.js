bc8685ed98c157952cfcc1420b5d768b09a0e71f
Jeffrey McCommas <jeffmccommas@gmail.com>
2013-11-17 18:06:08 -0500

    Completed the Functions Industry

--- Functions_Industry/js/scripts.js
+++ Functions_Industry/js/scripts.js
@@ -16,6 +16,7 @@ var pint = 16;
 var quart = 32;
 var final = final;
 
+// Function that converts the users input
 function converter(){
 
     if (units === "quart"){
@@ -32,25 +33,26 @@ function converter(){
     return final;
 }
 
-
+// User confirmation popup to give us a true or false value
 var answer = confirm("This is a simple Javascript program that will help you convert U.S. ounces to kitchen units.\n\nExample would be if you need to know how many Tablespoons 32 ounces is for your recipe.\n\nIf you wish to continue click the OK button. If not click cancel!!");
 
-
+// If user confirmation == true then do this next step
 if (answer === true) {
     var ounces = (parseFloat(prompt("Enter the amount of ounces you wish to convert. Example 64" ,64)));
 
     var units = prompt("Enter the unit you want to convert the ounces to.\n\nChoices are quart, pint, cup or tbsp", '');
 
-
+// Invoke the function
  converter();
 
    var results = (ounces + " Ounce(s) is equivalent to " + final + " " + units + "(s)")
 
-
+// Is user answer to the confirmation is false then we will do this step
 } else {
     alert("Thanks for visiting!! Bye!!")
 }
-
+// Print the results
+alert(results);
 console.log(results);
 
 
