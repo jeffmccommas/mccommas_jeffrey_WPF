c9b4f83423322003148c8d22b66f4915eb35be5a
Jeffrey McCommas <jeffmccommas@gmail.com>
2013-11-17 07:22:01 -0500

    Started the main Project

--- Functions_Industry/js/scripts.js
+++ Functions_Industry/js/scripts.js
@@ -3,3 +3,16 @@
  Nov. 16th 2013
  Activity: Functions Industry
  */
+
+
+/////////// Cooking Units Converter ////////////////////
+///////////// U.S. Kitchen Units ////////////
+
+var ounces;
+var pints;
+var cups;
+var tableSpoon;
+var teaSpoon;
+
+
+
